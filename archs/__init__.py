48d0dd3467536d6acda9945564ff03874753a830
root <1241062101@qq.com>
2024-11-01 10:13:27 +0800

    archs

--- archs/__init__.py
+++ archs/__init__.py
@@ -3,9 +3,13 @@ from os import path as osp
 
 from basicsr.utils import scandir
 
-# automatically scan and import arch modules for registry
-# scan all the files that end with '_arch.py' under the archs folder
+
+# Get the absolute path of the current file's directory
 arch_folder = osp.dirname(osp.abspath(__file__))
+
+# List comprehension to extract base names (without extensions) of files ending with '_arch.py'
 arch_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(arch_folder) if v.endswith('_arch.py')]
-# import all the arch modules
+
+# Dynamically import modules that match the naming convention (end with '_arch.py')
 _arch_modules = [importlib.import_module(f'archs.{file_name}') for file_name in arch_filenames]
+
